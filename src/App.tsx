95fea530261b8f58d578119a63af55cf700c5873
Natalya Botsman <botsmannatashaa@gmail.com>
2024-11-13 23:55:17 +0600

    Displayed all needed components & created main routing to switch between the pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import './App.css';
+import SearchForm from './Components/SearchForm/SearchForm.tsx';
+import Toolbar from './Components/Toolbar/Toolbar.tsx';
+import { Route, Routes } from 'react-router-dom';
+import Show from './Containers/Show/Show.tsx';
+import Home from './Containers/Home/Home.tsx';
 
 const App = () => {
-  return <></>;
+  return <>
+    <Toolbar/>
+    <div className="container m-5 ">
+      <SearchForm/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/shows/:showId" element={<Show/>}/>
+        <Route path="*" element={<h2 className="my-5 text-center">Page not found</h2>}/>
+      </Routes>
+    </div>
+  </>;
 };
 
 export default App;
